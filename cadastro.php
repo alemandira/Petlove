7bba88bcf14924d6a27770062d03fa0ba57919a1
alemandira <100322257+alemandira@users.noreply.github.com>
2023-04-10 21:45:32 -0300

    update

--- cadastro.php
+++ cadastro.php
@@ -1,2 +1,20 @@
+<?php
+    include("conexao.php");
 
-Os dados foram enviados com sucesso mané.....
+    $nome=$_POST['nome'];
+    $telefone=$_POST['telefone'];
+    $email=$_POST['email'];
+    $senha=$_POST['senha'];
+
+    $sql="INSERT INTO cadastro(nome, telefone, email, senha) VALUE ('$nome', '$telefone', '$email', '$senha')";
+
+    if(mysqli_query($conexao, $sql)){
+            echo"Usuário cadastrado com sucesso";
+    }
+    else{
+        echo "Erro".mysqli_connect_error($conexao);
+    }
+    mysqli_close($conexao);
+
+?>
+
